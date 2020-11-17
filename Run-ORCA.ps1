8c7e2b926e1f4129813bbc0babe0563901b6ce61
argetlam59 <shannon.skinn@me.com>
2020-11-18 08:33:00 +1000

    Add files via upload

---
+++ Run-ORCA.ps1
@@ -0,0 +1,62 @@
+﻿<# 
+This script is for installing and running ORCA report on a tenancy. 
+
+Office 365 Advanced Threat Protection Recommended Configuration Analyzer (ORCA)
+
+TODO
+Test
+
+Changelog: 
+
+1.0 - Release 17/11/2020
+    Created Script
+#>
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+Set-ExecutionPolicy Unrestricted -Force
+$msg = 'Do you need to install exchange powershell?'
+do {
+    choice /c yn /m $msg
+    $response = $LASTEXITCODE
+    if ($response -eq 1) {
+        Install-Module -Name ExchangeOnlineManagement
+        Pause
+        Update-Module -Name ExchangeOnlineManagement
+    }
+        $response = 2
+} until ($response -eq 2)
+
+$msg = 'Do you need to install ORCA for the first time?'
+do {
+    choice /c yn /m $msg
+    $response = $LASTEXITCODE
+    if ($response -eq 1) {
+        Install-Module -Name ORCA
+    }
+        $response = 2
+} until ($response -eq 2)
+cls
+
+Connect-ExchangeOnline
+Write-Host "
+
+Did you just get a bunch of red Errors? 
+
+That likely means you need to install exchange powershell again. Close this window and restart."
+Pause
+Get-ORCAReport
+Pause
+Disconnect-ExchangeOnline
+Set-ExecutionPolicy Restricted -Force
---
+++ Setup-Scan2Email-365.ps1
@@ -0,0 +1,57 @@
+﻿<# 
+This script is for disableing authenticated smtp from an email address so that you can setup scan to email for a printer. 
+
+This is not designed for an account with MFA; if you have an account that has MFA, setup an app password and use that for the scanner. 
+
+TODO
+Test
+
+Changelog: 
+
+1.1 - Relase - 18/11/2020
+    Added admin elevation. 
+    Added ex policy
+1.0 - Release 17/11/2020
+    Created Script
+#>
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+Set-ExecutionPolicy Unrestricted -Force
+$msg = 'Do you need to install exchange powershell?'
+do {
+    choice /c yn /m $msg
+    $response = $LASTEXITCODE
+    if ($response -eq 1) {
+        Install-Module -Name ExchangeOnlineManagement
+        Pause
+        Update-Module -Name ExchangeOnlineManagement
+    }
+        $response = 2
+} until ($response -eq 2)
+cls
+
+Connect-ExchangeOnline
+Write-Host "Did you just get a bunch of red Errors? That likely means you need to install exchange powershell again. Close this window and restart as admin."
+Enable-OrganizationCustomization
+Pause
+
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+$account = [Microsoft.VisualBasic.Interaction]::InputBox("UserName DKIM", "Enter the email address of the account for scan2email")
+
+Set-CASMailbox -Identity $account -SmtpClientAuthenticationDisabled $false
+
+Pause
+Disconnect-ExchangeOnline
+Set-ExecutionPolicy Restricted -Force
